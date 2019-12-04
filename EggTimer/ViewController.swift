60fe5fa6e2031675fc839ec8a728522086ee96a5
Yasin Shamrat <yshamrat@gmail.com>
2019-12-04 14:24:01 +0600

    Egg Timer Challenge

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-
-
+    var audioPLayer: AVAudioPlayer!
+    var time: Int?
+    var timer = Timer()
+    var counter = 0
+    var isRunning = false
+    
+    var url = Bundle.main.url(forResource: "alarm", withExtension: ".wav")
+    
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    
+    @IBAction func eggButton(_ sender: UIButton) {
+            if sender.currentTitle == "Soft" {
+                time = 5
+            }else if sender.currentTitle == "Medium" {
+                time = 10
+            }else {
+                time = 15
+            }
+            
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(cooking), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func cooking(){
+        if counter < time! {
+            counter += 1
+            progressView.progress = Float(counter) / Float(time!)
+            progressView.tintColor = UIColor.red
+        }else {
+            audioPLayer = try! AVAudioPlayer(contentsOf: self.url!)
+            audioPLayer.play()
+            timer.invalidate()
+            time = 0
+            counter = 0
+            progressView.progress = 0.0
+        
+        }
+        
+        
+    }
+    
 }
